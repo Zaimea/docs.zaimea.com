52c07536bb70d1f94d394908428f5c2a7e76f199
custura <127675571+custura@users.noreply.github.com>
2024-05-13 10:52:14 +0200

    wip

--- resources/views/_partials/nav-items.blade.php
+++ resources/views/_partials/nav-items.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col px-3 mt-4 space-y-6 lg:mt-0 lg:px-3 lg:items-center lg:space-y-0 lg:space-x-6 lg:flex-row">
     <a class="font-medium text-gray-600 dark:text-gray-200 hover:underline w-full"
-       href="{{ route('docs', '') }}">
+       href="{{ route('docs', 'introduction') }}">
         Docs
     </a>
     <div class="flex flex-row space-x-4 mt-20 lg:mt-0 items-center">
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,23 +8,23 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @php
-        $newTitle = empty($title) ? config('site.title') : "{$title} | ".config('site.title')
+        $newTitle = empty($title) ? config('site.title') : "{$title} | " . config('site.title');
     @endphp
 
-    <title>{{ $newTitle  }}</title>
+    <title>{{ $newTitle }}</title>
 
     <meta name="description" content="{{ $attributes->get('description') ?? config('site.description') }}">
 
-    <meta property="og:site_name" content="{{ config('site.title') }}"/>
-    <meta property="og:title" content="{{ $newTitle }}"/>
-    <meta property="og:description" content="{{ $description ?? config('site.description') }}"/>
-    <meta property="og:url" content="{{ url()->current() }}"/>
-    <meta property="og:image" content="https://docs.zaimea.com/assets/img/logo.png"/>
-    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="{{ config('site.title') }}" />
+    <meta property="og:title" content="{{ $newTitle }}" />
+    <meta property="og:description" content="{{ $description ?? config('site.description') }}" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+    <meta property="og:image" content="https://docs.zaimea.com/assets/img/logo.png" />
+    <meta property="og:type" content="website" />
 
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:creator" content="@enunomaduro">
-    <meta name="twitter:image:alt" content="Micro-framework for console applications">
+    <meta name="twitter:creator" content="@custura">
+    <meta name="twitter:image:alt" content="zaimea work scheduler">
 
     <link rel="icon" href="/favicon.ico">
 
@@ -32,46 +33,27 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @stack('styles')
+
 </head>
+
 <body {{ $attributes->except(['title', 'description']) }}>
 
     <div class="flex flex-col min-h-screen dark:bg-gray-900">
         {{ $slot }}
 
-        {{ $footer ?? "" }}
+        {{ $footer ?? '' }}
 
         <footer class="py-4 text-sm text-center text-gray-200 bg-gray-800" role="contentinfo">
             <ul class="flex flex-col justify-center my-0 list-none md:flex-row">
                 <li class="md:mr-2">
-                    &copy; <a href="https://docs.zaimea.com" class="font-bold text-white hover:text-white"
-                              title="Zaimea docs website">Zaimea Documentation</a> {{ date('Y') }}.
-                </li>
-
-                <li>
-                    Built with <a href="https://laravel.com" class="font-bold text-white hover:text-white" title="Laravel">Laravel</a>
-                    and <a href="https://tailwindcss.com" class="font-bold text-white hover:text-white"
-                           title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
-                    by <a href="https://custura.de" class="font-bold text-white hover:text-white" title="Custura">Custura</a>.
+                    &copy; {{ date('Y') }} Zaimea. All rights reserved.
                 </li>
             </ul>
         </footer>
     </div>
 
+    @stack('scripts')
 
-@stack('scripts')
-
-@if (app()->environment('production'))
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-61404619-3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-        gtag('config', 'UA-61404619-3');
-    </script>
-@endif
 </body>
+
 </html>
